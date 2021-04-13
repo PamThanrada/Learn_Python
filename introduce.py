1a3146817af43b6cb9efecb3bd424384923900e2
PamThanrada <pamthanradasonna@gmail.com>
2021-04-13 14:46:08 +0700

    initial commit

---
+++ introduce.py
@@ -0,0 +1,11 @@
+#Please introduce yourself
+
+print("Hello")
+print("What is your name?")
+MyName = input()
+print("Nice to meet you, " + MyName)
+print("The lenght of your name is:")
+print(len(MyName))
+print("What is your age?")
+MyAge = input()
+print("You will be" + str(int(MyAge) + 1) + "in next year")
