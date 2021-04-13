b555f98f610d89257d41c88abec006e803f52fa3
PamThanrada <pamthanradasonna@gmail.com>
2021-04-13 23:01:04 +0700

    add new files

---
+++ while-loop-example-name.py
@@ -0,0 +1,5 @@
+Password = ""
+while Password != "PPP":
+    print("Please input your correct password:")
+    Password = input()
+print("Thank you. You can access.")
---
+++ while-loop-example.py
@@ -0,0 +1,4 @@
+Num = 0
+while Num < 5:
+    print("Hello World!")
+    Num = Num +1
