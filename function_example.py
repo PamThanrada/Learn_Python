d8dd9dfad4b1b47d78418eea79688e94f2a9dbd4
PamThanrada <pamthanradasonna@gmail.com>
2021-04-16 22:31:09 +0700

    add new file

---
+++ function_example.py
@@ -0,0 +1,7 @@
+def hello():
+    print('Hello', end= '') #end= next line to be one line
+    print('Pam')
+    print('Hello', 'Hello', 'Hello')
+    print('Hello', 'Hello', 'Hello', sep= 'ABC' )
+
+hello()
