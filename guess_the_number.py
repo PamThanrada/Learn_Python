3adc661d37efc31ef7d6a057070ee6073ec2c94b
PamThanrada <pamthanradasonna@gmail.com>
2021-04-17 19:55:10 +0700

    add new lines

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,11 +2,11 @@ import random
 print('Hello, What is your name?')
 name = input()
 print('Ok ' + name + ' I am thinking of a number between 1 to 10.')
-print('Please, Guess the number.')
 
 secretNumber = random.randint(1, 10)
 
 for guessTaken in range(1, 8):       #You can guess 7 times.
+    print('Please, Guess the number')
     GuessNumber = int(input())
     if GuessNumber > secretNumber:
         print('It is high number.')
@@ -16,7 +16,7 @@ for guessTaken in range(1, 8):       #You can guess 7 times.
         break
 
 if GuessNumber == secretNumber:
-    print('True ' + name + ' You can guessed the number.')
+    print('True ' + name + ' You can guessed the number in ' + str(guessTaken) + ' gresses.')
 else:
-    print('The number I was thinking is ' + str(secretNumber))
+    print('The number I was thinking of was ' + str(secretNumber))
 
